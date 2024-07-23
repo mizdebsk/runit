9fd594f20c5a95209afc4d2cceef7fcf72fd4ae8
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-23 05:46:15 +0200

    Implement sourceRPM matcher

--- runit-api/src/main/java/io/kojan/runit/api/RUnit.java
+++ runit-api/src/main/java/io/kojan/runit/api/RUnit.java
@@ -1,6 +1,10 @@
 package io.kojan.runit.api;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import io.kojan.javadeptools.rpm.RpmInfo;
 
 public class RUnit {
     public static <T> void assertThat(String reason, T value, Matcher<? super T> matcher) {
@@ -13,5 +17,24 @@ public class RUnit {
         if (!matcher.matches(value)) {
             throw new FailedAssumption(reason, value, matcher);
         }
+    }
+
+    public static Matcher<RpmInfo> sourceRPM() {
+        return new TypeSafeMatcher<>() {
+            @Override
+            protected boolean matchesSafely(RpmInfo rpm) {
+                return rpm.isSourcePackage();
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("source RPM");
+            }
+
+            @Override
+            protected void describeMismatchSafely(RpmInfo rpm, Description description) {
+                description.appendText("was binary RPM ").appendValue(rpm);
+            }
+        };
     }
 }
