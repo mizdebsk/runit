1fe3c58a0c28eab9a509ca55b583b5e1c38b9cd4
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-31 10:40:25 +0200

    Document FailedExpectation

--- runit-api/src/main/java/io/kojan/runit/api/expectation/FailedExpectation.java
+++ runit-api/src/main/java/io/kojan/runit/api/expectation/FailedExpectation.java
@@ -2,10 +2,32 @@ package io.kojan.runit.api.expectation;
 
 import org.hamcrest.Matcher;
 
+/**
+ * Represents a failed expectation. A condition that was expected, but the
+ * actual value did not match the expectation. Typically implemented by
+ * throwable classes.
+ * 
+ * @author Mikolaj Izdebski
+ */
 public interface FailedExpectation {
+    /**
+     * Get Hamcrest {@link Matcher} representing expected condition.
+     * 
+     * @return matcher for expected value
+     */
     Matcher<?> getExpectationMatcher();
 
+    /**
+     * Get the actual unexpected value that was found.
+     * 
+     * @return actual unexpected value
+     */
     Object getActualValue();
 
+    /**
+     * Get additional message describing expectation mismatch.
+     * 
+     * @return additional message about the failed expectation
+     */
     String getMessage();
 }
