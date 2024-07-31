2b69f09f804102f5a9e75da8b05d5d3a245d255d
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-31 11:45:53 +0200

    Document GlobalContext

--- runit-api/src/main/java/io/kojan/runit/api/GlobalContext.java
+++ runit-api/src/main/java/io/kojan/runit/api/GlobalContext.java
@@ -5,8 +5,24 @@ import java.util.stream.Stream;
 
 import io.kojan.javadeptools.rpm.RpmPackage;
 
+/**
+ * Context in which RUnit tests are ran.
+ * <p>
+ * Specifies a set of RPM packages on which tests are ran.
+ */
 public interface GlobalContext {
+    /**
+     * Get list of packages on which tests are ran.
+     * 
+     * @return list of RPM packages
+     */
     List<RpmPackage> getRpmPackages();
 
+    /**
+     * Produces a {@link Stream} of {@link PackageContext}s that allows iterating
+     * over all packages in the global context.
+     * 
+     * @return stream of package contexts
+     */
     Stream<PackageContext> getPackageSubcontexts();
 }
