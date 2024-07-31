a4f10c7d8bd581b467f9afa8a1920fa43021d2bd
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-31 11:51:36 +0200

    Document FileContext

--- runit-api/src/main/java/io/kojan/runit/api/FileContext.java
+++ runit-api/src/main/java/io/kojan/runit/api/FileContext.java
@@ -4,10 +4,31 @@ import java.nio.file.Path;
 
 import io.kojan.javadeptools.rpm.RpmFile;
 
+/**
+ * Context in which file tests annotated with {@link FileTest} are ran.
+ * <p>
+ * Specifies the particular file contained in RPM package on which file tests
+ * are ran.
+ */
 public interface FileContext extends PackageContext {
+    /**
+     * Get path to the particular file on which file test is ran.
+     * 
+     * @return path to file inside RPM package
+     */
     Path getFilePath();
 
+    /**
+     * Get the particular file on which file test is ran.
+     * 
+     * @return the file
+     */
     RpmFile getRpmFile();
 
+    /**
+     * Get content of the particular file on which file test is ran.
+     * 
+     * @return byte content of the file
+     */
     byte[] getFileContent();
 }
