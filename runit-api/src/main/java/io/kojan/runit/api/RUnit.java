8170d169b39843898f9eccea284a77a95fb267b9
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-31 10:10:38 +0200

    Move matchers to io.kojan.runit.api.matcher

--- runit-api/src/main/java/io/kojan/runit/api/RUnit.java
+++ runit-api/src/main/java/io/kojan/runit/api/RUnit.java
@@ -1,13 +1,15 @@
 package io.kojan.runit.api;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 
 import io.kojan.javadeptools.rpm.RpmInfo;
 import io.kojan.runit.api.assertion.RUnitAssertions;
+import io.kojan.runit.api.matcher.RUnitMatchers;
 
 public class RUnit {
+    private RUnit() {
+    }
+
     public static <T> void assertThat(String reason, T value, Matcher<? super T> matcher) {
         RUnitAssertions.assertThat(reason, value, matcher);
     }
@@ -17,21 +19,6 @@ public class RUnit {
     }
 
     public static Matcher<RpmInfo> sourceRPM() {
-        return new TypeSafeMatcher<>() {
-            @Override
-            protected boolean matchesSafely(RpmInfo rpm) {
-                return rpm.isSourcePackage();
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("source RPM");
-            }
-
-            @Override
-            protected void describeMismatchSafely(RpmInfo rpm, Description description) {
-                description.appendText("was binary RPM ").appendValue(rpm);
-            }
-        };
+        return RUnitMatchers.sourceRPM();
     }
 }
