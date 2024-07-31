d84de0abe63875f8b55a83bdc59976f886794895
Mikolaj Izdebski <mizdebsk@redhat.com>
2024-07-31 11:54:04 +0200

    Document PackageType

--- runit-api/src/main/java/io/kojan/runit/api/PackageType.java
+++ runit-api/src/main/java/io/kojan/runit/api/PackageType.java
@@ -1,5 +1,13 @@
 package io.kojan.runit.api;
 
+/**
+ * Represents the kind of RPM package &ndash; source RPM (SRPM) or binary RPM.
+ */
 public enum PackageType {
-    SOURCE, BINARY, BOTH;
+    /** Represents a source RPM package (SRPM). */
+    SOURCE,
+    /** Represents a binary RPM package. */
+    BINARY,
+    /** Represents both source and binary RPM packages. */
+    BOTH;
 }
